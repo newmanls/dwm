4220c9095fde463145e65ed78cc6e6a6ecbc4ada
newmanls <newmansanchez189@gmail.com>
2023-03-10 10:05:58 -0400

    patch: notitle-20210715-138b405

--- config.h
+++ config.h
@@ -101,7 +101,6 @@ static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkStatusText,        0,              Button3,        spawn,          {.v = dmenucmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
